e8bb3f457e8c53ab466a72f327520ed89ec5f8a3
Luke Parker <moongateuk@gmail.com>
2018-11-29 19:27:59 +0000

    Initial Project

---
+++ src/UnityPackageMaker/Program.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="Program.cs" company="Luke Parker">
+// Copyright (c) Luke Parker 2018.
+// </copyright>
+
+namespace UnityPackageMaker
+{
+    using System;
+
+    /// <summary>
+    /// Main Program.
+    /// </summary>
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
